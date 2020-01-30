eaf9d04ca990bb4d5024ff8f9f23dbe61c3df069
Amr Sekilly <amr@pelcro.com>
2020-01-30 17:50:52 +0200

    Class issue

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-console.log("test");
+class LivingBeing {
+  constructor(name) {
+    this.name = name;
+  }
+
+  eat() {
+    console.log(`${this.name} eats`);
+  }
+
+  drink() {
+    console.log(`${this.name} drinks`);
+  }
+
+  sleep() {
+    console.log(`${this.name} sleeps`);
+  }
+
+  hibernate() {
+    console.log(`${this.name} hibernates`);
+  }
+}
+
+class Bear extends LivingBeing {
+  hibernate() {
+    console.log(`${this.name} hibernates`);
+  }
+}
+
+class Monkey extends LivingBeing {
+  jump() {
+    console.log(`${this.name} jumps`);
+  }
+}
+
+class Frog extends Monkey {}
+
+const bob = new Bear("Bob");
+const Monkeeyyyy = new Monkey("Monkeeyyyy");
+const Froggy = new Frog("Froggy");
+
+bob.drink();
+bob.hibernate();
+Froggy.hibernate();
+Froggy.jump();
+
+Monkeeyyyy.jump();
